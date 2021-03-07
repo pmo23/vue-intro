bef1577b29f0678f0092bde80be4b7ea037d3b7c
Junichi Inaba <xx.bb.xx.pmo@gmail.com>
2021-03-07 21:55:40 +0900

    class, style

---
+++ app-3.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        messages: [
+            { text: 'This is normal message.', error: false },
+            { text: 'This is ERROR message!', error: true },
+        ],
+        progress: 70,
+        message: 'Hello world'
+    }
+})
