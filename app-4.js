3bfa99e0ea4d417bab636b0cc4473d0e2de9e07d
Junichi Inaba <xx.bb.xx.pmo@gmail.com>
2021-03-07 22:12:55 +0900

    form

---
+++ app-4.js
@@ -0,0 +1,11 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: '',
+        description: 'This is textarea.',
+        framework: 'vue',
+        tools: { webpack: true, babel: false, yarn: false },
+        foods: ['🍩'],
+        lang: 'php'
+    }
+})
