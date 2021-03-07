86e54fd1601448a061c2e83326295bc2d04bfe81
Junichi Inaba <xx.bb.xx.pmo@gmail.com>
2021-03-07 20:23:56 +0900

    template

---
+++ app-2.js
@@ -0,0 +1,17 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        messages: {
+            count: 3
+        },
+        user: {
+            gender: 'male'
+        },
+        currentTab: 1,
+        frameworks: [
+            { name: 'Vue', creator: 'Evan You', stars: 79760 },
+            { name: 'React', creator: 'Facebook', stars: 85623 },
+            { name: 'Angular', creator: 'Google', stars: 31976 },
+        ]
+    }
+})
